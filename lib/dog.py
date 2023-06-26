8a2d5e706d7b801a4a1384e722d1cf39794adfd8
Macolister <bispo.mac@gmail.com>
2023-06-26 19:02:18 -0400

    interesting lab for sure

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,50 @@
 from models import Dog
+from sqlalchemy import (create_engine, desc,
+    Index, Column, DateTime, Integer, String)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-def create_table(base):
-    pass
+Base = declarative_base()
 
+def create_table(base, engine):
+    __tablename__ = 'dogs'
+
+    Index('index_name', 'name')
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    grade = Column(Integer())
+
+if __name__ == '__main__':
+
+    engine = create_engine('sqlite:///:memory:')
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
-
+    dogs = session.query(Dog).all()
+    return dogs
 def find_by_name(session, name):
-    pass
+    doggie = session.query(Dog).filter(Dog.name.like(f'{name}')).one()
+    # dog = session.query(Dog).filter(Dog.name == name).one()
+    # return dog
+    return doggie
 
 def find_by_id(session, id):
-    pass
+    doggie = session.query(Dog).filter(Dog.id.like(f'{id}')).one()
+    return doggie
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    query = session.query(Dog).filter(Dog.name.like(f'{name}'),
+        Dog.breed == breed).one()
+    return query
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
